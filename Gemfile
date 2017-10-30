94a6e3ff984e104b18e573d3361155a5e7010d22
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-10-30 14:38:39 -0400

    hide api key

--- Gemfile
+++ Gemfile
@@ -18,7 +18,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 
+
 group :development, :test do
+  gem 'dotenv-rails'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
--- app/controllers/api/v1/static_pages_controller.rb
+++ app/controllers/api/v1/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::StaticPagesController < ApplicationController
 skip_before_action :verify_authenticity_token
+
    def create
      state = (params[:name])
      response = HTTParty.get("https://api.yelp.com/v3/businesses/search?term=campgrounds&location=#{state}",
-     :headers => { "Authorization" => 'Content-Type' => 'application/json'})
+     :headers => { "Authorization" => "Bearer #{ENV["YELP_API_KEY"]}",  'Content-Type' => 'application/json'})
      @camp_array = JSON.parse(response.body)
      @campgrounds_list = @camp_array['businesses']
      render json: @campgrounds_list
