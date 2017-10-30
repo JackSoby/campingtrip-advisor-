1fa762809308d70f0c9df868a9826bb10ea1a801
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-10-30 14:11:38 -0400

    take out api key

--- app/controllers/api/v1/static_pages_controller.rb
+++ app/controllers/api/v1/static_pages_controller.rb
@@ -3,7 +3,7 @@ skip_before_action :verify_authenticity_token
    def create
      state = (params[:name])
      response = HTTParty.get("https://api.yelp.com/v3/businesses/search?term=campgrounds&location=#{state}",
-     :headers => { "Authorization" => "Bearer xABOrIYFA0EOp_EL9miyL5Fg-PPhq40RP-e5Ddl4jAXIOuun3v6aP3xwAya_lGYZaP1qGnqAW5ziINejcOz74KjjQMn9MJE7xWFTmb80GI1mT6HvxvU-o0YNG07rWXYx", 'Content-Type' => 'application/json'})
+     :headers => { "Authorization" => 'Content-Type' => 'application/json'})
      @camp_array = JSON.parse(response.body)
      @campgrounds_list = @camp_array['businesses']
      render json: @campgrounds_list
