78152830c85607a316f48122f8d920111034be6d
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-11-16 16:19:37 -0500

    added a README

---
+++ app/javascript/react/test/CampingtripAdvisor.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CampingtripAdvisor from '../src/CampingtripAdvisor';
+import { shallow } from 'enzyme'
+
+describe('CampingtripAdvisor', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <CampingtripAdvisor/>
+    )
+  })
+
+  it('should render NavBar components', () => {
+    expect(wrapper.find('NavBar').length).toEqual(1)
+  })
+
+
+})
