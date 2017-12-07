29ea819605b3c652133c6eedb094c52270cec2d4
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-12-07 11:16:46 -0500

    fix the format and clean up some code so its more readable

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -9,32 +9,26 @@ skip_before_action :verify_authenticity_token
       else
         render json: 'note did not save'
       end
-
    end
 
    def destroy
-
      Note.where(text: params[:text]).destroy_all
      camp = Campground.find(params[:id])
      note = camp.notes.where(user: current_user)
      render json: note
-
    end
 
    def update
-
      update_note = Note.find(params[:note_id])
      update_note.update(text: params[:note_text])
      camp= Campground.find(params[:id])
      note = camp.notes.where(user: current_user)
      render json: note
-
    end
 
 
 
   def note_params
      params.require(:note).permit(:text, :campground_id)
-
   end
 end
--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::ProfilesController < ApplicationController
 
   def index
     member = { id: current_user.id, campgrounds: current_user.campgrounds, first_name: current_user.first_name, last_name: current_user.last_name}
-      render json: member
-    end
+    render json: member
+  end
 end
